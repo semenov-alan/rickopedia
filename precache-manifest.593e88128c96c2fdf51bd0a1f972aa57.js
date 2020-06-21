66bc510b0ec9a99cb5a2b03cff2fca0a70c016a0
semenov-alan <ilia160297@gmail.com>
2020-06-22 00:32:40 +0300

    Updates

---
+++ precache-manifest.593e88128c96c2fdf51bd0a1f972aa57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9082f564992bba7a61801e03c6d1253",
+    "url": "/tensorProject/index.html"
+  },
+  {
+    "revision": "782f434b0a0b91b20490",
+    "url": "/tensorProject/static/css/main.cebca18c.chunk.css"
+  },
+  {
+    "revision": "00aee5e19348a4574bba",
+    "url": "/tensorProject/static/js/2.474c02da.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/tensorProject/static/js/2.474c02da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "782f434b0a0b91b20490",
+    "url": "/tensorProject/static/js/main.77aaf6a0.chunk.js"
+  },
+  {
+    "revision": "eb80dd365d337deacbfe",
+    "url": "/tensorProject/static/js/runtime-main.b2f4fbe1.js"
+  },
+  {
+    "revision": "a80683c828f5e535faeb66d160cb1d56",
+    "url": "/tensorProject/static/media/logo.a80683c8.svg"
+  }
+]);
